3cb270ebe956de8ac7a73cab1b258a1fab8ce088
Luke Hovington <lukeh@hovo.id.au>
2017-06-16 23:06:42 +1000

    Add form buttons

--- assets/js/loadPersonel.js
+++ assets/js/loadPersonel.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
                   if($.inArray( value[sectionLabel], Object.keys(namesBySection) ) == -1 )
                   {
                         namesBySection[value[sectionLabel]] = []
-                        console.log( value[nameLabel], namesBySection );
+                        //console.log( value[nameLabel], namesBySection );
                   }
                   namesBySection[value[sectionLabel]].push(value[nameLabel])
                   
@@ -36,7 +36,7 @@ $( document ).ready(function() {
     // Add all the sections to the page, and make them clickable
     function addSections(items) {
         $.each(Object.keys(items).sort(), function( index, value ) {
-            $('#sectionRadio').append('<input type="radio" name="section" value="'+value+'" />'+value+'<br/>');
+            $('#sectionRadio').append('<label><input type="radio" name="section" value="'+value+'" />'+value+'<br/></label>');
         });
         
         $("input[name='section']").on("click", function() {
@@ -50,12 +50,18 @@ $( document ).ready(function() {
         // Remove anyone already in the list from before, then add the new people
         $('#nameRadio').empty();
         $.each(namesBySection[sectionName], function( index, value ) {
-            $('#nameRadio').append('<input type="radio" name="section" value="'+value+'" />'+value+'<br/>');
+            $('#nameRadio').append('<label><input type="radio" name="sectionNames" value="'+value+'" />'+value+'</label><br/>');
         });
         
-        // $("input[name='section']").on("click", function() {
-        //     addNames($(this).val());
-        // });
+        $("input[name='sectionNames']").on("click", function() {
+            addButton();
+        });
+    }
+
+    function addButton() {
+        $('#buttonDiv').empty();
+        $('#buttonDiv').append('<button type="button" class="btn btn-success">Arrive</button>');
+        $('#buttonDiv').append('<button type="button" class="btn btn-danger">Leave</button>');
     }
 
     getCSV();
